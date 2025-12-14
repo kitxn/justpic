fdeff87663e0745bd33334a5292480f09c07dc9a
Александр <sasha-kvasnikov@mail.ru>
2025-12-14 19:05:23 +0300

    add unit tests for the Session model

--- src/models/sessions/internal.rs
+++ src/models/sessions/internal.rs
@@ -117,3 +117,101 @@ impl Session {
         time_left.num_days() as i16
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use chrono::{Days, Utc};
+
+    use crate::models::users::User;
+
+    use super::*;
+
+    fn create_session_for_test() -> Session {
+        let user = User::new("john_doe", "hunter42!".to_string());
+
+        Session::new(user.id_copy())
+    }
+
+    #[test]
+    fn test_session_model_creation() {
+        let user = User::new("john_doe", "hunter42!".to_string());
+
+        let item = Session::new(user.id_copy());
+
+        assert_eq!(
+            item.owner_id(),
+            user.id(),
+            "The session owner ID must match the user ID"
+        );
+
+        assert!(
+            !item.is_expired(),
+            "The new session must not have already expired"
+        );
+
+        let time_delta = Utc::now().timestamp() - item.created.timestamp();
+        assert!(
+            (-1..=5).contains(&time_delta),
+            "The creation time should roughly correspond to the current one"
+        );
+    }
+
+    #[test]
+    fn test_session_times() {
+        let mut item = create_session_for_test();
+
+        assert!(
+            item.created < item.expires,
+            "The session must not expire before it is created"
+        );
+
+        let expired_old = item.expires().timestamp();
+        item.extend_life_time(5);
+
+        assert!(
+            expired_old < item.expires().timestamp(),
+            "After life extension, its expiration time should be longer"
+        );
+    }
+
+    #[test]
+    fn test_session_expiration() {
+        let mut item = create_session_for_test();
+        item.expires = Utc::now() - Days::new(1);
+
+        assert!(
+            item.is_expired(),
+            "A session that expired a day ago should be marked as expired"
+        );
+
+        assert!(
+            matches!(item.throw_error_if_expired(), Err(Error::AccessDenied)),
+            "If the session has expired, it should return an ACCESS_DENIED error"
+        );
+
+        assert_eq!(
+            item.days_of_life_left(),
+            -1,
+            "For an expired session, the number of days remaining must be equal to -1"
+        );
+    }
+
+    #[test]
+    fn test_session_to_cookie() {
+        let item = create_session_for_test();
+
+        let cookie = item.as_cookie();
+
+        assert_eq!(
+            cookie.name(),
+            SESSION_COOKIE_NAME,
+            "The session cookie name must match the SESSION_COOKIE_NAME constant"
+        );
+
+        assert_eq!(
+            cookie.value().to_string(),
+            item.id.to_string(),
+            "The session cookie value must match the session key"
+        );
+    }
+}
